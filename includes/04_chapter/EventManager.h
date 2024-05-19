5ec39b56e43f66ba0abe98371b203f8b95ca166a
OSALD2000 <os14052020@gmail.com>
2024-05-19 16:45:45 +0200

    05_state

--- includes/04_chapter/EventManager.h
+++ includes/04_chapter/EventManager.h
@@ -122,3 +122,17 @@ private:
 
 
 
+//#include "Game.h"
+//
+//int main(int, char**) {
+//    // Program entry point.
+//    Game game; // Creating our game object.
+//    while (!game.GetWindow()->IsDone()) {
+//        // Game loop.
+//        game.Update();
+//        game.Render();
+//        game.RestartClock();
+//    }
+//
+//    return 0;
+//}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
-#include "includes/04_chapter/Game.h"
 
 int main(int, char**) {
-    // Program entry point.
-    Game game; // Creating our game object.
-    while (!game.GetWindow()->IsDone()) {
-        // Game loop.
-        game.Update();
-        game.Render();
-        game.RestartClock();
-    }
 
     return 0;
 }
